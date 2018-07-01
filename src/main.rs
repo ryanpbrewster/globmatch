bc7c75fe001d0a21798df7bfdb063cd9248bf521
Ryan P. Brewster <RyanPBrewster@gmail.com>
2018-06-30 22:11:55 -0700

    Give up on trie, back up. Maybe DFAs?

--- src/main.rs
+++ src/main.rs
@@ -5,44 +5,36 @@ use std::str::FromStr;
 
 fn main() -> Result<(), Box<Error>> {
     println!("Hello, world!");
-    let mut trie = Trie::default();
-    {
-        let stdin = std::io::stdin();
-        for line in stdin.lock().lines() {
-            let path: Path = line?.parse()?;
-            println!("{:?}", path);
-            println!("{:?}", overlaps(&trie, &path));
-            trie.insert(path);
-            println!("{:?}", trie);
-        }
+    let stdin = std::io::stdin();
+    for line in stdin.lock().lines() {
+        let path: Path = line?.parse()?;
+        println!("{:?}", path);
     }
     Ok(())
 }
 
-fn overlaps(trie: &Trie, path: &Path) -> Vec<Path> {
-    if path.is_empty() && trie.count > 0 {
-        return vec![Path::new()];
-    }
-
-    Vec::new()
-}
-
 #[derive(Debug, Eq, PartialEq, Clone, Ord, PartialOrd)]
 pub enum Fragment {
-    Glob,
-    Wildcard,
     Literal(String),
+    Wildcard,
+    Glob,
 }
 
 impl FromStr for Fragment {
     type Err = &'static str;
 
     fn from_str(s: &str) -> Result<Fragment, Self::Err> {
-        Ok(match s {
-            "*" => Fragment::Wildcard,
-            "**" => Fragment::Glob,
-            other => Fragment::Literal(other.to_owned()),
-        })
+        if s.is_empty() {
+            return Err("no empty fragments");
+        }
+        if s.contains('*') {
+            return match s {
+                "*" => Ok(Fragment::Wildcard),
+                "**" => Ok(Fragment::Glob),
+                other => Err("only * and ** fragments may contain *"),
+            };
+        }
+        Ok(Fragment::Literal(s.to_owned()))
     }
 }
 
@@ -59,26 +51,19 @@ impl FromStr for Path {
         Ok(Path(fragments))
     }
 }
+impl IntoIterator for Path {
+    type Item = Fragment;
+    type IntoIter = std::vec::IntoIter<Fragment>;
+
+    fn into_iter(self) -> <Self as IntoIterator>::IntoIter {
+        self.0.into_iter()
+    }
+}
 impl Path {
     fn new() -> Path {
         Path(Vec::new())
     }
     fn is_empty(&self) -> bool {
         self.0.is_empty()
-    }
-}
-
-#[derive(Debug, Default)]
-pub struct Trie {
-    children: BTreeMap<Fragment, Trie>,
-    count: usize,
-}
-
-impl Trie {
-    fn insert(&mut self, path: Path) {
-        let mut cur = path.0.into_iter().fold(self, |cur, fragment| {
-            cur.children.entry(fragment).or_insert_with(Trie::default)
-        });
-        cur.count += 1;
     }
 }
